2db0accf91a4e6b43862eef384c2047a2dc41015
Christoph <christoph.tonnier@telent.de>
2017-12-14 11:30:28 +0100

    extending typings

--- dist/LeafletPlayback.d.ts
+++ dist/LeafletPlayback.d.ts
@@ -3,5 +3,8 @@ import * as L from 'leaflet';
 declare module 'leaflet' {
   class Playback {
     constructor(map: Map, geoJSON: Object, callback: any, options: any);
+    isPlaying(): boolean;
+    setCursor(ms: Number|String);
+	stop();
   }
 }
